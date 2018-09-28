add41574ccd8b97738017b78450c236079895051
zy840055899 <840055899@qq.com>
2018-09-28 11:45:17 +0800

    案件类型，船舶类型

--- src/main/java/com/jinge/ocean/dao/DictionaryDao.java
+++ src/main/java/com/jinge/ocean/dao/DictionaryDao.java
@@ -1,6 +1,8 @@
 package com.jinge.ocean.dao;
 
 import com.jinge.ocean.entity.dictionary.CaseFrom;
+import com.jinge.ocean.entity.dictionary.CaseType;
+import com.jinge.ocean.entity.dictionary.ShipType;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -20,6 +22,24 @@ public interface DictionaryDao {
     void updateCaseFrom(CaseFrom caseFrom);
 
     List<CaseFrom> getAllCaseFrom(@Param("caseFromName") String caseFromName);
+
+    /*----------------------caseType 案件类型增删改查------------------*/
+    void addCaseType(CaseType caseType);
+
+    void delCaseTypeById(@Param("caseTypeId") String caseTypeId);
+
+    void updateCaseType(CaseType caseType);
+
+    List<CaseType> getAllCaseType(@Param("caseTypeName") String caseTypeName);
+
+    /*----------------------shipType 主体类型（船舶）增删改查------------------*/
+    void addShipType(ShipType shipType);
+
+    void delShipTypeById(@Param("shipTypeId") String shipTypeId);
+
+    void updateShipType(ShipType shipType);
+
+    List<ShipType> getAllShipType(@Param("shipTypeName") String shipTypeName);
 
 
 }
---
+++ src/main/java/com/jinge/ocean/entity/dictionary/ShipType.java
@@ -0,0 +1,31 @@
+package com.jinge.ocean.entity.dictionary;
+
+public class ShipType {
+    private int id;
+    private String shipType;
+    private String remark;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
