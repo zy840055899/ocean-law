8771fae0a032f5ca76862b8e578524b5b01b73d5
zy840055899 <840055899@qq.com>
2018-09-29 17:29:56 +0800

    被检查人员增删改查

---
+++ src/main/java/com/jinge/ocean/entity/Person.java
@@ -0,0 +1,108 @@
+package com.jinge.ocean.entity;
+
+
+public class Person {
+
+  private int id;
+  private String name;
+  private String idNumber;
+  private String shipName;
+  private String gender;
+  private String age;
+  private String job;
+  private String certificateType;
+  private String phone;
+  private String addr;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getIdNumber() {
+    return idNumber;
+  }
+
+  public void setIdNumber(String idNumber) {
+    this.idNumber = idNumber;
+  }
+
+
+  public String getShipName() {
+    return shipName;
+  }
+
+  public void setShipName(String shipName) {
+    this.shipName = shipName;
+  }
+
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+
+  public String getJob() {
+    return job;
+  }
+
+  public void setJob(String job) {
+    this.job = job;
+  }
+
+
+  public String getCertificateType() {
+    return certificateType;
+  }
+
+  public void setCertificateType(String certificateType) {
+    this.certificateType = certificateType;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  public String getAddr() {
+    return addr;
+  }
+
+  public void setAddr(String addr) {
+    this.addr = addr;
+  }
+
+
+
+}
