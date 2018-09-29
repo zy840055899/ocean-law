98d03d70ff3fb4f26201f3822af86ec7aee83a05
zy840055899 <840055899@qq.com>
2018-09-29 14:22:30 +0800

    properties值获取，坑多，复原

--- src/main/java/com/jinge/ocean/controller/ShipController.java
+++ src/main/java/com/jinge/ocean/controller/ShipController.java
@@ -1,6 +1,9 @@
 package com.jinge.ocean.controller;
 
 import com.jinge.ocean.entity.Ship;
+import com.jinge.ocean.utils.FileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 
 /**
  * @author zheng y 2018-9-27 11:09:24
@@ -16,6 +18,7 @@ import java.io.File;
 @Controller
 @RequestMapping("ship")
 public class ShipController {
+    static Logger logger = LoggerFactory.getLogger(ShipController.class);
 
     @RequestMapping("toAddShip")
     public String toAddShip(Model model) {
@@ -25,10 +28,7 @@ public class ShipController {
     @RequestMapping("addShip")
     @ResponseBody
     public String addShip(Model model, Ship ship, MultipartFile file, HttpServletRequest request) throws Exception {
-        model.addAttribute("ship", ship);
-
-
-
-        return "ship/addShip";
+        String result = new FileUtil().uploadMultipartFile(file);
+        return result;
     }
 }
--- src/main/java/com/jinge/ocean/utils/FileUtil.java
+++ src/main/java/com/jinge/ocean/utils/FileUtil.java
@@ -1,18 +1,20 @@
 package com.jinge.ocean.utils;
 
-import com.jinge.ocean.interceptor.LoginInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 
+@Component
 public class FileUtil {
     static Logger logger = LoggerFactory.getLogger(FileUtil.class);
 
+    private String filePath = "/Users/";
 
-    public static String uploadMultipartFile(MultipartFile file){
+    public String uploadMultipartFile(MultipartFile file) {
         if (file != null) {
             String path;// 文件路径
             String type;// 文件类型
@@ -22,19 +24,18 @@ public class FileUtil {
             if (!StringUtils.isEmpty(type)) {
                 if ("GIF".equals(type.toUpperCase()) || "PNG".equals(type.toUpperCase()) || "JPG".equals(type.toUpperCase())) {
                     // 项目在容器中实际发布运行的根路径
-//                    String realPath = request.getSession().getServletContext().getRealPath("/");
-                    String realPath = "/Users/";
+//                    String filePath = request.getSession().getServletContext().getRealPath("/");
                     // 自定义文件名称
                     String trueFileName = String.valueOf(System.currentTimeMillis()) + fileName;
                     // 设置存放图片文件的路径
-                    path = realPath +/*System.getProperty("file.separator")+*/trueFileName;
+                    path = filePath +/*System.getProperty("file.separator")+*/trueFileName;
                     // 转存文件到指定的路径
                     try {
                         file.transferTo(new File(path));
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         logger.warn("文件上传异常");
                         path = "文件上传异常";
-                    }finally {
+                    } finally {
                         return path;
                     }
                 } else {
