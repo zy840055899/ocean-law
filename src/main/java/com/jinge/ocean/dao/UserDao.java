0c22aa335c946e7ebcd1f49ec982de765b700300
zy840055899 <840055899@qq.com>
2018-09-30 15:07:31 +0800

    用户管理

---
+++ src/main/java/com/jinge/ocean/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.jinge.ocean.dao;
+
+import com.jinge.ocean.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author zheng 2018-9-27 16:29:09
+ */
+public interface UserDao {
+    void addUser(User user);
+
+    void delUserById(@Param("userId") String userId);
+
+    void updateUser(User user);
+
+    List<User> getAllUsers(@Param("username") String username);
+
+}
